dc4ecedcf978b728ee8d9cefe5de6b973e088ffb
Morne34 <50170999+Morne34@users.noreply.github.com>
2025-02-14 02:01:41 +0200

    setting_cors_removed

--- taskManagement/settings.py
+++ taskManagement/settings.py
@@ -19,8 +19,7 @@ ALLOWED_HOSTS = [
     'blumaps.com',
     '127.0.0.1',
     'localhost',
-    '952b-41-198-143-102.ngrok-free.app'  # Added ngrok URL
-    "http://localhost:3000",
+    
 ]
 
 INSTALLED_APPS = [
@@ -31,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TaskManagementApp',
-    "corsheaders"
+ 
    # Add your apps here
 ]
 
@@ -43,7 +42,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'taskManagement.urls'
